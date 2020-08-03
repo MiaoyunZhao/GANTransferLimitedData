d1a6df5ce0a00bad696060c415c39691c74cdad3
Miaoyun Zhao <miaoyun9zhao@gmail.com>
2020-08-03 11:40:03 -0400

    Update __init__.py

--- gan_training/models/__init__.py
+++ gan_training/models/__init__.py
@@ -5,12 +5,12 @@ from gan_training.models import (
 
 generator_dict = {
     'resnet2': resnet2.Generator,
-    'resnet2_contral_kernel': resnet2_contral_kernel.Generator,
+    'resnet2_AdaFM': resnet2_AdaFM.Generator,
 }
 
 discriminator_dict = {
     'resnet2': resnet2.Discriminator,
-    'resnet2_contral_kernel': resnet2_contral_kernel.Discriminator,
+    'resnet2_AdaFM': resnet2_AdaFM.Discriminator,
 }
 
 encoder_dict = {
